82143bcde4401d7eb9b44f2af864a0bf07cede62
Terra Roush <terra.roush@gmail.com>
2021-06-09 16:00:22 -0500

    fetch call working for recipes

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,17 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 
 const Table = () => {
- 
+  const [recipes, setRecipes] = useState([]);
+  const columns = [
+    { field: "title", header: "Title" },
+    { field: "summary", header: "Summary" },
+    { field: "readyInMinutes", header: "Ready In Minutes" },
+    { field: "vegetarian", header: "Vegetarian" },
+  ];
 
-  return (
-   <DataTable>
-      <Column field="code" header="Code"></Column>
-      <Column field="name" header="Name"></Column>
-      <Column field="category" header="Category"></Column>
-      <Column field="quantity" header="Quantity"></Column>
-    </DataTable>
+  const APIKey = process.env.REACT_APP_SPOONACULAR_KEY;
+
+  const getRecipes = () => {
+    return fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${APIKey}&number=4`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setRecipes(data);
+      });
+  };
+  useEffect(() => {
+    getRecipes();
+    console.log(recipes);
+  }, []);
+  console.log(recipes);
+
+  const dynamicColumns = columns.map((col, i) => {
+    return <Column key={col.field} field={col.field} header={col.header} />;
+  });
+
+  return recipes ? (
+    <div>
+      <div className="card">
+        <DataTable value={recipes}>{dynamicColumns}</DataTable>
+      </div>
+    </div>
+  ) : (
+    ""
   );
 };
 
