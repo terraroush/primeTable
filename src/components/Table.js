eb584093866abe491b517204fcb97bf101c24519
Terra Roush <terra.roush@gmail.com>
2021-06-10 11:40:56 -0500

    added img tag dynamically and sized

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,42 +1,68 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "./Table.css";
 
 const Table = () => {
   const [recipes, setRecipes] = useState([]);
+
+  const recipeImageTemplate = (rowData) => {
+    return (
+      <img
+        src={`${rowData.image}`}
+        alt={`${rowData.title}`}
+        className="recipeImage"
+      />
+    );
+  };
+
   const columns = [
+    { field: "image", header: "Image", body: { recipeImageTemplate } },
     { field: "title", header: "Title" },
-    { field: "summary", header: "Summary" },
     { field: "readyInMinutes", header: "Ready In Minutes" },
-    { field: "vegetarian", header: "Vegetarian" },
+    { field: "servings", header: "Servings" },
   ];
 
   const APIKey = process.env.REACT_APP_SPOONACULAR_KEY;
 
   const getRecipes = () => {
     return fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${APIKey}&number=4`
+      `https://api.spoonacular.com/recipes/random?apiKey=${APIKey}&number=20`
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         setRecipes(data);
       });
   };
   useEffect(() => {
     getRecipes();
-    console.log(recipes);
   }, []);
   console.log(recipes);
 
   const dynamicColumns = columns.map((col, i) => {
-    return <Column key={col.field} field={col.field} header={col.header} />;
+    if (col.field === "image") {
+      return (
+        <Column
+          key={col.field}
+          field={col.field}
+          body={recipeImageTemplate}
+          header={col.header}
+        />
+      );
+    } else {
+      return <Column key={col.field} field={col.field} header={col.header} />;
+    }
   });
 
   return recipes ? (
     <div>
       <div className="card">
-        <DataTable value={recipes}>{dynamicColumns}</DataTable>
+        <DataTable value={recipes.recipes} sortMode="multiple">
+          {dynamicColumns}
+        </DataTable>
       </div>
     </div>
   ) : (
