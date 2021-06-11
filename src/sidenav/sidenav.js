5a313e91fd0f6fc47a3b57a2c6dab77482d84ff7
Terra Roush <terra.roush@gmail.com>
2021-06-11 15:36:33 -0500

    navbar in effect and linked up. no style yet

--- src/sidenav/sidenav.js
+++ src/sidenav/sidenav.js
@@ -1,7 +1,20 @@
+import { SideNav, SideNavItem, SideNavLink } from "carbon-components-react";
 import React from "react";
 
 const Sidenav = () => {
-  return <div>Side navigation</div>;
+  return (
+    <div className="sidenav-container">
+      <SideNav
+        isFixedNav
+        expanded={true}
+        isChildOfHeader={false}
+        aria-label="Side navigation"
+      >
+        <SideNavLink href="/recipe-table">Random Recipes</SideNavLink>
+        <SideNavLink href="/flowers">Flowers</SideNavLink>
+      </SideNav>
+    </div>
+  );
 };
 
 export default Sidenav;
